8857bc28f13dcb028c74af9abd92aea738bda72f
tcordina <gitlab@tablar.ovh>
2026-03-25 15:08:54 +0100

    kubernetes: move infrastructure/ content to apps/ dir

--- kubernetes/.bootstrap/bootstrap.sh
+++ kubernetes/.bootstrap/bootstrap.sh
@@ -6,6 +6,7 @@ source $DIR/.env
 
 echo -e "\nBootstrapping cluster..."
 
+helm repo update
 helmfile apply --skip-diff-on-install --suppress-diff --file $DIR/helmfile.d/00-crds.yaml
 BW_TOKEN=$BW_TOKEN helmfile apply --skip-diff-on-install --suppress-diff --file $DIR/helmfile.d/01-secrets.yaml.gotmpl
 helmfile apply --skip-diff-on-install --suppress-diff --file $DIR/helmfile.d/02-apps.yaml
