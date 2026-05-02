8863fbde1c44740d84c1f8ee3242f0742d7a6a99
tcordina <gitlab@tablar.ovh>
2026-05-02 16:26:46 +0200

    bootstrap: update procedure to work with both main and staging environments

--- kubernetes/bootstrap/bootstrap.sh
+++ kubernetes/bootstrap/bootstrap.sh
@@ -6,7 +6,5 @@ source "${DIR}/../../.env"
 
 echo -e "\nBootstrapping cluster..."
 
-helm repo update
-helmfile apply --skip-diff-on-install --suppress-diff --file "${DIR}/helmfile.d/00-crds.yaml"
-BW_TOKEN="${BW_TOKEN}" helmfile apply --skip-diff-on-install --suppress-diff --file "${DIR}/helmfile.d/01-secrets.yaml.gotmpl"
-helmfile apply --skip-diff-on-install --suppress-diff --file "${DIR}/helmfile.d/02-apps.yaml"
+BW_TOKEN="${BW_TOKEN}" helmfile apply --kube-context "${KUBE_CONTEXT}" --skip-diff-on-install --suppress-diff --file "${DIR}/helmfile.d/00-base.yaml.gotmpl"
+helmfile apply --kube-context "${KUBE_CONTEXT}" --skip-diff-on-install --suppress-diff --file "${DIR}/helmfile.d/01-flux.yaml.gotmpl"
