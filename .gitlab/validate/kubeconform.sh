60ca493f09faff9a176081067fbc3120e9eaaf79
tcordina <gitlab@tablar.ovh>
2026-07-25 23:21:27 +0200

    CI/validate: use flux build instead of kustomize

--- .gitlab/validate/kubeconform.sh
+++ .gitlab/validate/kubeconform.sh
@@ -6,8 +6,6 @@ KUBERNETES_DIR=$1
 
 [[ -z "${KUBERNETES_DIR}" ]] && echo "Kubernetes location not specified" && exit 1
 
-kustomize_args=("--load-restrictor=LoadRestrictionsNone")
-kustomize_config="kustomization.yaml"
 kubeconform_args=(
     "-strict"
     "-ignore-missing-schemas"
@@ -20,22 +18,40 @@ kubeconform_args=(
     "-verbose"
 )
 
-echo "=== Validating kustomizations in ${KUBERNETES_DIR}/clusters ==="
-find "${KUBERNETES_DIR}/clusters" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
-do
-    echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
-    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | kubeconform "${kubeconform_args[@]}"
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+validate() {
+    local name=$1
+    local ks_file=$2
+    local path=$3
+
+    echo "INFO - Validating ${ks_file}"
+    flux build kustomization "$name" \
+        --path "$path" \
+        --kustomization-file "$ks_file" \
+        --dry-run \
+        | kubeconform "${kubeconform_args[@]}"
+
+    if [[ ${PIPESTATUS[0]} != 0 || ${PIPESTATUS[1]} != 0 ]]; then
+        echo "ERROR - Validation failed for ${ks_file}"
         exit 1
     fi
+}
+
+echo "INFO - Validating kustomizations in ${KUBERNETES_DIR}/clusters"
+find "${KUBERNETES_DIR}/clusters" -type f -name "cluster.yaml" -print0 | while IFS= read -r -d $'\0' file;
+do
+    dir=$(dirname "$file")
+    name=$(basename "$dir")
+    validate "apps" "$file" "$dir"
 done
 
-echo "=== Validating kustomizations in ${KUBERNETES_DIR}/apps ==="
-find "${KUBERNETES_DIR}/apps" -type f -name $kustomize_config -not -path "**/unused/*" -print0 | while IFS= read -r -d $'\0' file;
+echo "INFO - Validating kustomizations in ${KUBERNETES_DIR}/apps"
+find "${KUBERNETES_DIR}/apps" -type f -name "ks.yaml" -not -path "**/unused/*" -print0 | while IFS= read -r -d $'\0' file;
 do
-    echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
-    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | kubeconform "${kubeconform_args[@]}"
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-        exit 1
-    fi
+    ks_dir=$(dirname "$file")
+    name=$(basename "$ks_dir")
+    app_path="${ks_dir}/app"
+
+    [[ ! -d "$app_path" ]] && continue
+
+    validate "$name" "$file" "$app_path"
 done
